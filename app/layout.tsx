d80821a7f11b6cd6df03c44e98a424eb50ed3d64
Not-Sarthak <notsarthakshah@gmail.com>
2025-01-12 21:41:25 +0530

    feat: migrate branding from muse to digitalhippo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Muse",
+  title: "DigitalHippo",
   description: "",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "/favicon.ico",
-        href: "/favicon.ico",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/favicon.ico",
-        href: "/favicon.ico",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
@@ -29,11 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <head></head>
+      <body
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <Navbar />
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1"> {children}</div>
+        </main>
+      </body>
     </html>
   );
 }
